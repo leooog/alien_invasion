273dc142c8b17c37def0c33aa4d4fc7b55f40579
zhanglei <leooog@gmail.com>
2022-07-15 15:52:21 +0800

    加入子弹，并正常运行

--- settings.py
+++ settings.py
@@ -13,6 +13,6 @@ class Settings():
         self.bullet_speed_factor = 1
         self.bullet_width = 3
         self.bullet_height =15
-        self.bullet_color = 60,60,60
+        self.bullet_color = (60,60,60)
 
         
