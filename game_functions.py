3ebb8ab88a2aa1c86628f20ca155de639d2e69aa
zhanglei <leooog@gmail.com>
2022-07-17 16:02:47 +0800

    完成子弹，下面开始添加外星人。

--- game_functions.py
+++ game_functions.py
@@ -12,9 +12,14 @@ def check_keydown_events(event,ai_settings,screen,ship,bullets):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        #创建一颗子弹，并将其加入到编组bullets中
-        new_bullet = Bullet(ai_settings,screen,ship)
-        bullets.add(new_bullet)
+        fire_bullet(ai_settings,screen,ship,bullets)
+
+def fire_bullet(ai_settings,screen,ship,bullets):
+    """如果还没有到达限制，就发射一颗子弹"""
+    #创建子弹，并将其加入到编组bullets中
+    if len(bullets) < ai_settings.bullets_allowed:
+            new_bullet = Bullet(ai_settings,screen,ship)
+            bullets.add(new_bullet)
 
 
 def check_keyup_events(event,ship):
@@ -36,6 +41,17 @@ def check_events(ai_settings,screen,ship,bullets):
         elif event.type == pygame.KEYUP:
             check_keyup_events(event,ship)
 
+def update_bullets(bullets):
+    """更新子弹的位置，并删除已经消失的子弹"""
+    #更新子弹的位置
+    bullets.update()
+
+    #删除已经消失的子弹
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+                bullets.remove(bullet)
+
+
 def update_screen(ai_settings,screen,ship,bullets):
     """更新屏幕上的图像，并切换到新屏幕"""
     # 每次循环时都重绘屏幕
@@ -49,5 +65,3 @@ def update_screen(ai_settings,screen,ship,bullets):
 
     #让最近绘制的屏幕可见
     pygame.display.flip()
-
-    #无用的注释，测试git
